a1240c8f964a4ff73b55060de1e8d4207566b2b4
Angel Martin Antezana Flores <a.antezana@empresasim.com>
2019-05-21 06:35:31 -0400

    Creating createLineaController

---
+++ src/controllers/CreateLineaController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import views.ListLineaView;
+
+/**
+ *
+ * @author skypper
+ */
+public class CreateLineaController {
+
+    ListLineaView listLineaView;
+
+    public CreateLineaController(ListLineaView view) {
+        this.listLineaView = view;
+        loadConf();
+    }
+
+    public void loadConf() {
+
+    }
+}
