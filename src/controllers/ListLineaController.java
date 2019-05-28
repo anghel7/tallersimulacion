590d6c95916fe3e5820108c108de0df930cb7b6c
Angel Martin Antezana Flores <a.antezana@empresasim.com>
2019-05-28 10:30:25 -0400

    Refactor listLineaController

--- src/controllers/ListLineaController.java
+++ src/controllers/ListLineaController.java
@@ -5,7 +5,15 @@
  */
 package controllers;
 
+import java.util.ArrayList;
+import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import models.Linea;
 import models.ModelManager;
+import views.LineaItemView;
 import views.ListLineaView;
 
 /**
@@ -24,6 +32,60 @@ public class ListLineaController {
     }
 
     public void loadConf() {
+        listLineaView.jScrollPaneContainer = new javax.swing.JScrollPane();
+        listLineaView.panelContainer = new javax.swing.JPanel();
+        listLineaView.setPreferredSize(new java.awt.Dimension(850, 550));
+        listLineaView.setRequestFocusEnabled(false);
 
+        listLineaView.jScrollPaneContainer.setBorder(javax.swing.BorderFactory.createTitledBorder("Lineas"));
+
+        javax.swing.GroupLayout panelContainerLayout = new javax.swing.GroupLayout(listLineaView.panelContainer);
+        listLineaView.panelContainer.setLayout(panelContainerLayout);
+
+        ArrayList<LineaItemView> itemViewList = new ArrayList<LineaItemView>();
+        for (Linea linea : modelManager.getLineas()) {
+            LineaItemView lineaItemView = new LineaItemView();
+            itemViewList.add(lineaItemView);
+        }
+
+
+        /*Horizontal*/
+        GroupLayout.Group groupHorizontal = panelContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING);
+        for (LineaItemView component : itemViewList) {
+            groupHorizontal
+                    .addComponent(component, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE);
+
+        }
+
+        panelContainerLayout.setHorizontalGroup(groupHorizontal);
+        /*Vertical*/
+        GroupLayout.Group groupVertical = panelContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING);
+
+        GroupLayout.SequentialGroup sqGroup = panelContainerLayout.createSequentialGroup();
+        for (LineaItemView component : itemViewList) {
+            sqGroup.addComponent(component, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED);
+        }
+        groupVertical.addGroup(sqGroup);
+        panelContainerLayout.setVerticalGroup(groupVertical);
+
+        listLineaView.jScrollPaneContainer.setViewportView(listLineaView.panelContainer);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(listLineaView);
+        listLineaView.setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addContainerGap(186, Short.MAX_VALUE)
+                                .addComponent(listLineaView.jScrollPaneContainer, javax.swing.GroupLayout.PREFERRED_SIZE, 500, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(164, 164, 164))
+        );
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addContainerGap(32, Short.MAX_VALUE)
+                                .addComponent(listLineaView.jScrollPaneContainer, javax.swing.GroupLayout.PREFERRED_SIZE, 483, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(25, 25, 25))
+        );
     }
 }
